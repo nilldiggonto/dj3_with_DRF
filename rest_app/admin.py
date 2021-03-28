1b44e11914a58d5503b8495106a26d24153f9212
nilldiggonto <nilldiggonto@gmail.com>
2021-03-29 00:29:41 +0600

    Form admin validation

--- rest_app/admin.py
+++ rest_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Status
+from .forms import StatusForm
 # Register your models here.
 @admin.register(Status)
 class StatusAdmin(admin.ModelAdmin):
     list_display=['user','content','timestamp','updated']
+    form = StatusForm
+    # class Meta:
+    #     model = Stat
