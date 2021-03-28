a4643c84b8215a9595b32cbfb17bf4be2bfc097d
nilldiggonto <nilldiggonto@gmail.com>
2021-03-28 22:51:02 +0600

    Custome Serializer

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import list_view,ListAPIview,ListAPIview2
+from .views import (list_view,ListAPIview,ListAPIview2, SerailizedListview,SerailizedDetailview,)
 
 urlpatterns = [
     path('list/',list_view,name='rest-list'),
     path('list/one/',ListAPIview.as_view(),name='rest-list-one'),
-    path('list/two/',ListAPIview2.as_view(),name='rest-list-two')
+    path('list/two/',ListAPIview2.as_view(),name='rest-list-two'),
+
+    path('list/serialize/',SerailizedListview.as_view(),name='rest-list-serializer'),
+    path('list/serialize/detail/',SerailizedDetailview.as_view(),name='rest-detail-serializer')
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
+from django.core.serializers import serialize
 
 # Create your views here.
 from .models import UpdateImage
@@ -42,3 +43,37 @@ class ListAPIview2(ListAPIviewmixins,View):
         }
 
         return self.render_to_json_response(data)
+
+
+####### SERIALIZER VIEW
+# class SerailizedDetailview(ListAPIviewmixins,View):
+
+
+class SerailizedListview(View):
+    def get(self,request,*args,**kwargs):
+        qs = UpdateImage.objects.all()
+        data = serialize('json',qs,fields=('user','content'))
+        # data = {
+        #     'user':obj.user.username,
+        #     'content':obj.content
+
+        # }
+        # json_data = json.dumps(data)
+        json_data = data
+        return HttpResponse(json_data,content_type='application/json')
+        # return self.render_to_json_response(data)
+class SerailizedDetailview(View):
+    def get(self,request,*args,**kwargs):
+        obj = UpdateImage.objects.get(id=1)
+        data = serialize('json',[obj,],fields=('user,content'))
+        # data = {
+        #     'user':obj.user.username,
+        #     'content':obj.content
+
+        # }
+        json_data = json.dumps(data)
+        json_data = data
+        return HttpResponse(json_data,content_type='application/json')
+        # return self.render_to_json_response(data)
+
+
