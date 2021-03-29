560b555108f5f1460e4b7b384b31db713807be5c
nilldiggonto <nilldiggonto@gmail.com>
2021-03-29 19:42:30 +0600

    board app serailizer

---
+++ board/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Sprint
+
+class SprintSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sprint
+        fields = ['id','name','description','endtime']
+        
--- board/views.py
+++ board/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Sprint
+from .serializer import SprintSerializer
 # Create your views here.
+from rest_framework import viewsets
+
+class SprintViewApi(viewsets.ModelViewSet):
+    queryset = Sprint.objects.order_by('-endtime')
+    serializer_class = SprintSerializer
