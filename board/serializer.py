c4c81827dc9ed04cc8b3ccafdfbf2ee8f3b12d0f
nilldiggonto <nilldiggonto@gmail.com>
2021-04-01 12:05:52 +0600

    Task and user view done

--- board/serializer.py
+++ board/serializer.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
-from .models import Sprint
+from .models import Sprint,Task
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source ='get_full_name', read_only=True)
+    class Meta:
+        model = User
+        fields = ('id',User.USERNAME_FIELD,'full_name','is_active')
+
 
 class SprintSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,5 +17,15 @@ class SprintSerializer(serializers.ModelSerializer):
 
 
 #creating custom mixins with DRF
-class DefaultMixins(object):
-    pass
+class TaskSerializer(serializers.ModelSerializer):
+
+    assigned = serializers.SlugRelatedField(slug_field=User.USERNAME_FIELD,read_only=True)
+
+
+    status_display = serializers.SerializerMethodField('get_status_display')
+    class Meta:
+        model = Task
+        fields = ['id','name','description','sprint','status_display','status','order','assigned','started','due','completed']
+
+    def get_status_display(self,obj):
+        return obj.get_status_display()
--- board/views.py
+++ board/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from .models import Sprint
-from .serializer import SprintSerializer
+from .models import Sprint,Task
+from .serializer import SprintSerializer,TaskSerializer,UserSerializer
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets,authentication,permissions
+from django.contrib.auth.models import User
 
-class SprintViewApi(viewsets.ModelViewSet):
+
+#creating a custom mixins
+class DefaultMixins(object):
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication
+    )
+    permission_classes = (permissions.IsAuthenticated,)
+
+    paginated_by =25
+    paginated_by_param = 'page_size'
+    max_paginated_by = 100
+class SprintViewApi(DefaultMixins,viewsets.ModelViewSet):
     queryset = Sprint.objects.order_by('-endtime')
     serializer_class = SprintSerializer
+
+
+############ TASK
+class TaskViewSet(DefaultMixins,viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+#userview Serializer
+class UserViewSet(DefaultMixins,viewsets.ReadOnlyModelViewSet):
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
