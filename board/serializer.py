973594a14eb028404975fb3419d40bba596623a0
nilldiggonto <nilldiggonto@gmail.com>
2021-04-02 10:50:35 +0600

    API Router and links

--- board/serializer.py
+++ board/serializer.py
@@ -1,18 +1,37 @@
 from rest_framework import serializers
 from .models import Sprint,Task
 from django.contrib.auth.models import User
+from rest_framework.reverse import reverse
 
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(source ='get_full_name', read_only=True)
+    links = serializers.SerializerMethodField('get_links')
+
     class Meta:
         model = User
-        fields = ('id',User.USERNAME_FIELD,'full_name','is_active')
+        fields = ('id',User.USERNAME_FIELD,'full_name','is_active','links')
+    
+    def get_links(self, obj):
+        request = self.context['request']
+        username = obj.get_username()
+        return {
+        'self': reverse('user-detail',
+        kwargs={User.USERNAME_FIELD: username}, request=request),
+        }
 
 
 class SprintSerializer(serializers.ModelSerializer):
+    links = serializers.SerializerMethodField('get_links')
+
     class Meta:
         model = Sprint
-        fields = ['id','name','description','endtime']
+        fields = ['id','name','description','endtime','links']
+    
+    def get_links(self,obj):
+        request = self.context['request']
+        return {
+            'self':reverse('sprint-detail',kwargs={'pk':obj.pk},request=request)
+        }
         
 
 
@@ -23,9 +42,26 @@ class TaskSerializer(serializers.ModelSerializer):
 
 
     status_display = serializers.SerializerMethodField('get_status_display')
+    links = serializers.SerializerMethodField('get_links')
+
     class Meta:
         model = Task
-        fields = ['id','name','description','sprint','status_display','status','order','assigned','started','due','completed']
+        fields = ['id','name','description','sprint','status_display','status','order','assigned','started','due','completed','links']
 
     def get_status_display(self,obj):
         return obj.get_status_display()
+    
+    def get_links(self,obj):
+        request = self.context['request']
+        links = {
+            'self':reverse('task-detail',kwargs={'pk':obj.pk},request=request),
+            'sprint':None,
+            'assigned':None
+        }
+
+        if obj.sprint_id:
+            links['sprint'] = reverse('sprint-detail',kwargs={'pk':obj.sprint_id},request=request)
+        if obj.assigned:
+            links['assigned'] = reverse('user-detail',kwargs={User.USERNAME_FIELD:obj.assigned},request=request)
+
+        return links
--- board/urls.py
+++ board/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import SprintViewApi
+from .views import SprintViewApi,TaskViewSet,UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'sprints', SprintViewApi)
-# router.register(r'tasks', views.TaskViewSet)
-# router.register(r'users', views.UserViewSet)
+router.register(r'tasks', TaskViewSet)
+router.register(r'users', UserViewSet)
 
 # urlpatterns = [
 #     # path('sprint/',SprintViewApi.as_view(),name='sprit-list'),
