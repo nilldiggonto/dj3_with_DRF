61a9acfc052731d2cb281f9e3b77e37a1fb33370
nilldiggonto <nilldiggonto@gmail.com>
2021-04-02 11:26:29 +0600

    Filter Added

--- board/views.py
+++ board/views.py
@@ -5,6 +5,8 @@ from .serializer import SprintSerializer,TaskSerializer,UserSerializer
 from rest_framework import viewsets,authentication,permissions
 from django.contrib.auth.models import User
 
+from rest_framework import authentication,permissions,viewsets,filters
+
 
 #creating a custom mixins
 class DefaultMixins(object):
@@ -17,15 +19,25 @@ class DefaultMixins(object):
     paginated_by =25
     paginated_by_param = 'page_size'
     max_paginated_by = 100
+
+    filter_backends =(
+        # filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
 class SprintViewApi(DefaultMixins,viewsets.ModelViewSet):
     queryset = Sprint.objects.order_by('-endtime')
     serializer_class = SprintSerializer
+    search_fields = ('name',)
+    ordering_fields = ('endtime','name')
 
 
 ############ TASK
 class TaskViewSet(DefaultMixins,viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    search_fields = ('name', 'description', )
+    ordering_fields = ('name', 'order', 'started', 'due', 'completed', ) 
 
 #userview Serializer
 class UserViewSet(DefaultMixins,viewsets.ReadOnlyModelViewSet):
@@ -33,6 +45,8 @@ class UserViewSet(DefaultMixins,viewsets.ReadOnlyModelViewSet):
     lookup_url_kwarg = User.USERNAME_FIELD
     queryset = User.objects.order_by(User.USERNAME_FIELD)
     serializer_class = UserSerializer
+
+    search_fields = (User.USERNAME_FIELD, )
 
 
     #do the router test
